cd737cbe8c22affe7f54c1b61129e99cd9fba92f
Carlos Alfonzo Lopez <carlos.alfonzo@gmail.com>
2018-09-11 16:40:04 -0700

    saving work

--- js/main.js
+++ js/main.js
@@ -10,6 +10,15 @@ function getMovies(searchText){
 	axios.get('http://www.omdbapi.com?t='+searchText+'&?i=tt3896198&apikey=41f46d7d')
 	.then((response) => {
 		console.log(response);
+		let movies = response.data.Search;
+		let output = '';
+		$.each(movies, (index, movie) => {
+			output += `
+			<div class="col-md-3">
+				<div class="well text-center">
+					<img src="$"
+		`;
+		});
 	})
 	.catch((err) => {
 		console.log(err);
