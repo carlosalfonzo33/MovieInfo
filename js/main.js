65bef04dcdc143936379df2600b83044d1fe01ec
Carlos Alfonzo Lopez <carlos.alfonzo@gmail.com>
2018-09-11 14:53:35 -0700

    fixed the axios query string in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(() => {
 	$('#searchForm').on('submit', (e) => {
-		console.log($('#searchText').val());
+		let searchText = $('#searchText').val();
+		getMovies(searchText);
 		e.preventDefault();
 	});
 });
+
+function getMovies(searchText){
+	axios.get('http://www.omdbapi.com?t='+searchText+'&?i=tt3896198&apikey=41f46d7d')
+	.then((response) => {
+		console.log(response);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+}
